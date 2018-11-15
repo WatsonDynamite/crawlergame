d5accaf0ae0b19f5c404e4ccd414fdba3468fc08
WatsonDynamite <2150665@my.ipleiria.pt>
2018-11-15 10:40:14 +0000

    first commit

---
+++ Assets/CameraMouseControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMouseControl : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+    private float rotZ = 0.0f;
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.4f);
+        transform.rotation = localRotation;
+    }
+}
---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public float speed = 2f;
+    public float runSpeed = 5f;
+    public float turnSmoothing = 15f;
+
+    public Camera camera;
+
+    public Animator animator;
+
+    public CharacterController characterController;
+    
+
+    private Vector3 movement;
+    private Rigidbody playerRigidBody;
+
+    void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float lh = Input.GetAxisRaw("Horizontal");
+        float lv = Input.GetAxisRaw("Vertical");
+
+        Move(lh, lv);
+    }
+
+
+    void Move(float lh, float lv)
+    {
+        movement.Set(lh, 0f, lv);
+
+        movement = camera.transform.TransformDirection(movement);
+        movement.y = 0f;
+
+        /*
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement = movement.normalized * runSpeed * Time.deltaTime;
+        }
+        else */
+        {
+            movement = movement.normalized * speed * Time.deltaTime;
+        }
+
+        playerRigidBody.MovePosition(transform.position + movement);
+
+        
+
+        if (lh != 0f || lv != 0f)
+        {
+            Rotating(lh, lv);
+        }
+    }
+
+
+    void Rotating(float lh, float lv)
+    {
+        Vector3 targetDirection = new Vector3(lh, 0f, lv);
+
+        Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+
+        Quaternion newRotation = Quaternion.Lerp(GetComponent<Rigidbody>().rotation, targetRotation, turnSmoothing * Time.deltaTime);
+
+        GetComponent<Rigidbody>().MoveRotation(newRotation);
+
+    }
+
+}
